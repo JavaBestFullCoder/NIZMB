bd990f52b841b373a2e946a80151e251f95e0e78
Mr. Talsorian <wotgamer111007@gmail.com>
2026-07-21 16:44:12 +0500

    NEW COM

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ import pytz
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 BOT_TOKEN = os.getenv("BOT_TOKEN", "6702099427:AAGXB9buUhOYWL1i5ui0QQiow93ZhV6GUf0")
-HEAD_OFFICE_CODE = "GlavnyOffice111"
+HEAD_OFFICE_CODE = "900911147Ns"
 TZ_NAME = "Asia/Tashkent"
 TZ = pytz.timezone(TZ_NAME)
 DB_PATH = os.path.join(BASE_DIR, "bot.db")
